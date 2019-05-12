d5501a401936d7da8d11621615326831b26bc789
ttrsato <tatsurosatoh@gmail.com>
2019-05-13 08:02:00 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -1,8 +1,3 @@
-enum Motor {
-    CH0 = 0,
-    CH1 = 1
-}
-
 enum StepAngleFixed {
     //% block="0.9"
     StepAngle0p9 = 0,
@@ -86,7 +81,7 @@ namespace lv8548step {
     }
 
     //% blockId=lv8548step_setstepangle block="Set motor angle/step to %deg"
-    export function setStepAngle(deg: number): void {
+    export function setStepAngle(deg: StepAngleFixed): void {
         let bufr = pins.createBuffer(8);
         bufr.setNumber(NumberFormat.UInt8LE, 0, 0xA5)
         bufr.setNumber(NumberFormat.UInt8LE, 1, 0xFF)
@@ -94,10 +89,10 @@ namespace lv8548step {
         bufr.setNumber(NumberFormat.UInt8LE, 3, 0x69)
         let fangle = 0x00000000
         switch (deg) {
-            case 0:
+            case StepAngleFixed.StepAngle0p9:
                 fangle = 0x3F666666
                 break
-            case 1:
+            case StepAngleFixed.StepAngle1p8:
                 fangle = 0x3FE66666
                 break
             case 2:
@@ -122,15 +117,14 @@ namespace lv8548step {
         serial.writeBuffer(bufr)
     }
 
-    //% blockId=lv8548step_motorrotationdeg block="Rotate %deg|degree (%freq|Hz), direction%cwccw, Excitation %exc "
-    //% duty.min=0 duty.max=100
-    export function motorRotationDeg(freq: number, deg: number, cwccw: RotorDir, exc: Excitation) : void {
-        
+    //% blockId=lv8548step_motorrotationdeg block="Deg%deg|Freq%freq|Dir%cwccw|Excitation%exc"
+    export function motorRotationDeg(freq: number, deg: number, cwccw: RotorDir, exc: Excitation): void {
+
         let bufr = pins.createBuffer(14);
         bufr.setNumber(NumberFormat.UInt8LE, 0, 0xA5)
         bufr.setNumber(NumberFormat.UInt8LE, 1, 0xFF)
         bufr.setNumber(NumberFormat.UInt8LE, 2, 0x03)
-        bufr.setNumber(NumberFormat.UInt8LE, 3, 0x69)
+        bufr.setNumber(NumberFormat.UInt8LE, 3, 0x6A)
         for (let i = 0; i < 4; i++) {
             bufr.setNumber(NumberFormat.UInt8LE, i + 4, (freq >> i * 8) & 0xFF)
         }
@@ -139,6 +133,64 @@ namespace lv8548step {
         }
         bufr.setNumber(NumberFormat.UInt8LE, 12, cwccw)
         bufr.setNumber(NumberFormat.UInt8LE, 13, exc)
+        serial.writeBuffer(bufr)
+    }
+
+    //% blockId=lv8548step_motorrotationtime block="Time%time|Freq%freq|Dir%cwccw|Excitation%exc"
+    export function motorRotationTime(freq: number, time: number, cwccw: RotorDir, exc: Excitation): void {
+
+        let bufr = pins.createBuffer(14);
+        bufr.setNumber(NumberFormat.UInt8LE, 0, 0xA5)
+        bufr.setNumber(NumberFormat.UInt8LE, 1, 0xFF)
+        bufr.setNumber(NumberFormat.UInt8LE, 2, 0x03)
+        bufr.setNumber(NumberFormat.UInt8LE, 3, 0x6B)
+        for (let i = 0; i < 4; i++) {
+            bufr.setNumber(NumberFormat.UInt8LE, i + 4, (freq >> i * 8) & 0xFF)
+        }
+        for (let i = 0; i < 4; i++) {
+            bufr.setNumber(NumberFormat.UInt8LE, i + 8, (time >> i * 8) & 0xFF)
+        }
+        bufr.setNumber(NumberFormat.UInt8LE, 12, cwccw)
+        bufr.setNumber(NumberFormat.UInt8LE, 13, exc)
+        serial.writeBuffer(bufr)
+    }
+
+    //% blockId=lv8548step_motorrotationstep block="Step%step|Freq%freq|Dir%cwccw|Excitation%exc"
+    export function motorRotationStep(freq: number, step: number, cwccw: RotorDir, exc: Excitation): void {
+
+        let bufr = pins.createBuffer(14);
+        bufr.setNumber(NumberFormat.UInt8LE, 0, 0xA5)
+        bufr.setNumber(NumberFormat.UInt8LE, 1, 0xFF)
+        bufr.setNumber(NumberFormat.UInt8LE, 2, 0x03)
+        bufr.setNumber(NumberFormat.UInt8LE, 3, 0x6C)
+        for (let i = 0; i < 4; i++) {
+            bufr.setNumber(NumberFormat.UInt8LE, i + 4, (freq >> i * 8) & 0xFF)
+        }
+        for (let i = 0; i < 4; i++) {
+            bufr.setNumber(NumberFormat.UInt8LE, i + 8, (step >> i * 8) & 0xFF)
+        }
+        bufr.setNumber(NumberFormat.UInt8LE, 12, cwccw)
+        bufr.setNumber(NumberFormat.UInt8LE, 13, exc)
+        serial.writeBuffer(bufr)
+    }
+
+    //% blockId=lv8548step_motorrotationhold block="Hold stepper"
+    export function motorRotationHold(): void {
+        let bufr = pins.createBuffer(4);
+        bufr.setNumber(NumberFormat.UInt8LE, 0, 0xA5)
+        bufr.setNumber(NumberFormat.UInt8LE, 1, 0xFF)
+        bufr.setNumber(NumberFormat.UInt8LE, 2, 0x03)
+        bufr.setNumber(NumberFormat.UInt8LE, 3, 0x6E)
+        serial.writeBuffer(bufr)
+    }
+
+    //% blockId=lv8548step_motorrotationfree block="Free stepper"
+    export function motorRotationFree(): void {
+        let bufr = pins.createBuffer(4);
+        bufr.setNumber(NumberFormat.UInt8LE, 0, 0xA5)
+        bufr.setNumber(NumberFormat.UInt8LE, 1, 0xFF)
+        bufr.setNumber(NumberFormat.UInt8LE, 2, 0x03)
+        bufr.setNumber(NumberFormat.UInt8LE, 3, 0x6F)
         serial.writeBuffer(bufr)
     }
 
